34bafd61d53bcf5a2ce861823e3541c92432f1d6
Nitesh Choubey <89741235+anshmoon@users.noreply.github.com>
2023-01-02 22:54:24 +0530

    Update urls.py

--- miniblog/urls.py
+++ miniblog/urls.py
@@ -30,3 +30,5 @@ urlpatterns = [
     path('delete/<int:id>/', views.delete_post, name='deletepost'),
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
